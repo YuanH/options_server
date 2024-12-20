30c5a77e3a1b6d3232401f47df28aeb73d364054
Yuan Huang <yuan.huang10@gmail.com>
2024-12-20 13:26:18 -0500

    creating vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,70 +1,70 @@
-# # terraform/main.tf (continued)
+# terraform/main.tf (continued)
 
-# # Create VPC
-# resource "aws_vpc" "main" {
-#   cidr_block           = var.vpc_cidr
-#   enable_dns_support   = true
-#   enable_dns_hostnames = true
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-#   tags = {
-#     Name = "${var.project_name}-vpc"
-#   }
-# }
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
 
-# # Create Internet Gateway
-# resource "aws_internet_gateway" "gw" {
-#   vpc_id = aws_vpc.main.id
+# Create Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
 
-#   tags = {
-#     Name = "${var.project_name}-gw"
-#   }
-# }
+  tags = {
+    Name = "${var.project_name}-gw"
+  }
+}
 
-# # Create Public Subnets
-# resource "aws_subnet" "public" {
-#   count                   = length(var.public_subnets)
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = var.public_subnets[count.index]
-#   availability_zone       = data.aws_availability_zones.available.names[count.index]
-#   map_public_ip_on_launch = true
+# Create Public Subnets
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
 
-#   tags = {
-#     Name = "${var.project_name}-public-subnet-${count.index + 1}"
-#   }
-# }
+  tags = {
+    Name = "${var.project_name}-public-subnet-${count.index + 1}"
+  }
+}
 
-# # Create Private Subnets
-# resource "aws_subnet" "private" {
-#   count             = length(var.private_subnets)
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = var.private_subnets[count.index]
-#   availability_zone = data.aws_availability_zones.available.names[count.index]
+# Create Private Subnets
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
-#   tags = {
-#     Name = "${var.project_name}-private-subnet-${count.index + 1}"
-#   }
-# }
+  tags = {
+    Name = "${var.project_name}-private-subnet-${count.index + 1}"
+  }
+}
 
-# # Create Route Table for Public Subnets
-# resource "aws_route_table" "public" {
-#   vpc_id = aws_vpc.main.id
+# Create Route Table for Public Subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.gw.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 
-#   tags = {
-#     Name = "${var.project_name}-public-rt"
-#   }
-# }
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
 
-# # Associate Route Table with Public Subnets
-# resource "aws_route_table_association" "public_assoc" {
-#   count          = length(aws_subnet.public)
-#   subnet_id      = aws_subnet.public[count.index].id
-#   route_table_id = aws_route_table.public.id
-# }
+# Associate Route Table with Public Subnets
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
 
-# # Data source for Availability Zones
-# data "aws_availability_zones" "available" {}
+# Data source for Availability Zones
+data "aws_availability_zones" "available" {}
