e0ce1e2934de6cae9212749bd233972712cb348a
Yuan Huang <yuan.huang10@gmail.com>
2024-12-20 13:47:12 -0500

    change target group type to ip

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -80,15 +80,15 @@ resource "aws_lb_target_group" "flask_tg" {
   port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  target_type = "ip"
 
   health_check {
-    interval            = 30
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
     healthy_threshold   = 5
     unhealthy_threshold = 2
-    matcher             = "200-299"
+    timeout             = 3
+    interval            = 30
+    path                = "/health"
+    protocol            = "HTTP"
   }
 
   tags = {
