b80470dd0bf746f0b85c9c54cbf112a59b9bb26b
Yuan Huang <yuan.huang10@gmail.com>
2026-02-12 15:56:45 -0500

    add lifecycle block for alb_cert

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,9 +1,12 @@
 
 resource "aws_acm_certificate" "alb_cert" {
-  domain_name               = "options.${var.domain_name}"  
+  domain_name               = "options.${var.domain_name}"
   subject_alternative_names = ["oura.${var.domain_name}"]
   validation_method         = "DNS"
 
+  lifecycle {
+    create_before_destroy = true
+  }
   tags = {
     Name = "${var.project_name}-alb-cert"
   }
